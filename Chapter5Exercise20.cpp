57e0e44c2daf491ec411eb92556425c8faf7993c
BryanH <Bryan_c_hurley@outlook.com>
2025-04-20 22:59:58 -0400

    Created Branch

--- Chapter5Exercise20.cpp
+++ Chapter5Exercise20.cpp
@@ -11,13 +11,13 @@ This program is a basic number guessing game with menu.
 #include <random>
 using namespace std;
 
-// Get menu selection
+// Menu
 int getMenuChoice() {
     int choice = 0;
     bool ran = false;
     while (choice < 1 || choice > 4) {
         if (ran == true) {
-            cout << "Please choose 1, 2, 3, or 4" << endl;
+            cout << "Please Enter 1, 2, 3, or 4" << endl;
         }
         cout << "1. Display Sessions\n2. Play Game\n3. Delete Sessions\n4. Exit\nChoice: ";
         cin >> choice;
@@ -26,10 +26,12 @@ int getMenuChoice() {
     return choice;
 }
 
-//  Run guessing game and save session
+// Game with guess counter
 void playGame() {
     string name;
     int guess = 0;
+    int tries = 0;
+
     random_device rd;
     default_random_engine eng(rd());
     uniform_int_distribution<int> dist(1, 100);
@@ -41,21 +43,22 @@ void playGame() {
     while (guess != secret) {
         cout << "Guess (1–100): ";
         cin >> guess;
+        tries++;
         if (guess < secret) cout << "Too low.\n";
         else if (guess > secret) cout << "Too high.\n";
     }
 
     ofstream file("sessions.txt", ios::app);
-    file << name << " guessed correctly.\n";
+    file << name << " guessed correctly in " << tries << " tries.\n";
     file.close();
-    cout << "Correct!\n";
+    cout << "Correct in " << tries << " tries!\n";
 }
 
-// Display or delete sessions
+// Sessions
 void showSessions() {
     ifstream file("sessions.txt");
     string line;
-    cout << "\n-- Session Log --\n";
+    cout << "\n-- Sessions --\n";
     while (getline(file, line)) {
         cout << line << endl;
     }
@@ -69,13 +72,11 @@ void deleteSessions() {
 
 int main() {
     int choice;
-
     do {
         choice = getMenuChoice();
         if (choice == 1) showSessions();
         else if (choice == 2) playGame();
         else if (choice == 3) deleteSessions();
-        
     } while (choice != 4);
 
     return 0;
